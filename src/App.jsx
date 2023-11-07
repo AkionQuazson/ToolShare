436da4053b2a63208a703c63585e1d4a83434c8e
AkionQuazson <111464265+AkionQuazson@users.noreply.github.com>
2023-11-07 05:35:14 -0700

    working on Register

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,31 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 import './App.css'
 import {BrowserRouter , Routes , Route} from 'react-router-dom'
+import Home from './Home';
+import {Login, Register} from './Auth';
 
 function App() {
-  const onmount = () => {
-    const options = {
-      url: '/api/listing',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json;charset=utf-8'
-      }
-    }
-    axios.get('/api/listing').then((res) => {
-      console.log(res);
-    });
-  }
-  useEffect(onmount, [])
+  // const onmount = () => {
+  //   const options = {
+  //     url: '/api/listing',
+  //     headers: {
+  //       'Accept': 'application/json',
+  //       'Content-Type': 'application/json;charset=utf-8'
+  //     }
+  //   }
+  //   axios.get('/api/listing').then((res) => {
+  //     console.log(res);
+  //   });
+  // }
+  // useEffect(onmount, [])
 
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route exact path='/' element={<Home/>} />
-          <Route exact path='/login' element={<Auth/>} />
+          <Route exact path='/login' element={<Login/>} />
+          <Route exact path='/register' element={<Register/>} />
         </Routes>
       </BrowserRouter>
     </>
